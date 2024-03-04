cf3f2c9e2d8b14a3076b090183ab6857bc6f6aa4
nont5 <116797801+nont5@users.noreply.github.com>
2024-03-04 19:46:15 +0200

    Create login.ts

---
+++ login.ts
@@ -0,0 +1,8 @@
+let loginData = {
+  Username: prompt("Enter a username: "),
+  Password: prompt("Enter a password: ")
+}
+
+if (loginData.Username == loginData.Username && loginData.Password == loginData.Password) {
+  console.log("Logged in as " + str(loginData.Username))
+}
